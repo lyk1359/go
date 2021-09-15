e48fd18fb16747fbb9afed01a25081b147ed8ad9
lyk1359 <lyk1359@naver.com>
2021-09-15 21:15:02 +0900

    v2.go

--- syntax04.go
+++ syntax04.go
@@ -1,5 +1,3 @@
-
-
 package main
 
 import(
@@ -7,8 +5,8 @@ import(
 	"math/rand"
 	"time" 
 )
-//난수 추출된 수의 소수 판정 프로그램 v0.1
-//소수: 1과 자기자신 외에는 나누어 떨어지지 않는 수(0과 1제외)
+//난수 추출된 수의 소수 판정 프로그램 v0.2
+
 func main(){
 	
 	seed :=time.Now().Unix()
@@ -18,15 +16,16 @@ func main(){
 	number := rand.Intn(150)+2 //0과 1제외,2~151 사이의 수
 	fmt.Println("임의로 추출된 수: ",number)
 	
-	for i:=1; i <= number; i++{
+	for i:=2; i <= number; i++{ //1과 number일때 loop돌지 않음
 		if number%i == 0{
-			count++
+			count=count+1
 		}
 	}
-	if count==2{
+	if count==0{
 		fmt.Println(number, "는(은) 소수입니다!")
 	}else{
 		fmt.Println(number, "는(은) 소수가 아닙니다~")
 	}
 }
+
 
